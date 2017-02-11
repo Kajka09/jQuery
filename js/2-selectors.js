4ca9c8e4ed6d77dab577b1368f7d93a9a7bcce88
Kajka09 <kajkastefaniak@gmail.com>
2017-02-11 09:55:23 +0100

    selektory

---
+++ js/2-selectors.js
@@ -0,0 +1,16 @@
+
+$(function(){
+	'use strict';
+	
+//	pobieranie elementów po znaczniku
+	var paragraphs = $('p');
+	console.log(paragraphs);
+	
+//	pobieranie elementów po id
+	var paragrapgOne = $('#paragraf');
+	console.log(paragrapgOne);
+	
+//	pobieranie elementów po klasie
+	var paragraphTwo = $('.paragraf');
+	console.log(paragraphTwo);
+});
